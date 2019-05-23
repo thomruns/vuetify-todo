a3182d9c4f50e7e82d016c6059d427d17e0a0d62
Thom David <thomdavid74@gmail.com>
2019-05-23 11:26:40 -0700

    Delete boilerplate code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,13 @@
 <template>
-  <HelloWorld />
+  <div class="home">
+    <h1>This is the home page</h1>
+  </div>
+  
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
+  
   export default {
-    components: {
-      HelloWorld
-    }
+
   }
 </script>
