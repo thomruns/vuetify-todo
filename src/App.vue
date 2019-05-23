c05a05237131aaaa9c4e3479047a8836201e17b0
Thom David <thomdavid74@gmail.com>
2019-05-23 14:21:03 -0700

    Add top toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <template>
-  <v-app>
-    
+  <v-app class="grey lighten-4">
+    <Navbar></Navbar>
     <v-content>
       <router-view></router-view>
     </v-content>
-
   </v-app>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar'
 
 export default {
+ 
   name: 'App',
+  components: {
+    Navbar
+  },
   data () {
     return {
-      //
+
     }
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1>This is the home page</h1>
+    <h1>Homepage</h1>
   </div>
   
 </template>
