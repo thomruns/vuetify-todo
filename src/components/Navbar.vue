2c0e6552bd61642a7f5c6be5371437bff4e9f42e
Thom David <thomdavid74@gmail.com>
2019-05-27 10:35:45 -0700

    Drawer menu list output

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,5 @@
 <template>
   <nav>
-
     <v-toolbar flat app>
       <v-toolbar-side-icon class="grey--text" @click="drawer=!drawer"></v-toolbar-side-icon>
       <v-toolbar-title class="text-uppercase grey--text">
@@ -13,8 +12,20 @@
         <v-icon right>exit_to_app</v-icon>
       </v-btn>
     </v-toolbar>
+    
 
-    <v-navigation-drawer app v-model="drawer" class="primary"></v-navigation-drawer>
+    <v-navigation-drawer app v-model="drawer" class="primary">
+      <v-list>
+        <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-tile-action>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+           <v-list-tile-title class="white--text">{{ link.text }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
 
   </nav>
 </template>
@@ -23,7 +34,12 @@
   export default {
     data() {
       return {
-        drawer: false
+        drawer: false,
+        links: [
+        { icon: 'dashboard', text: 'Dashboard', route: '/' },
+        { icon: 'folder', text: 'My Projects', route: '/projects' },
+        { icon: 'person', text: 'Team', route: '/team' },
+        ]
       }
     }
 
