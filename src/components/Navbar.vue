2070226fa5fa421929e4b75c069849b1c7e5315c
Thom David <thomdavid74@gmail.com>
2019-05-27 10:02:27 -0700

    Add drawer element

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,8 @@
 <template>
   <nav>
+    
     <v-toolbar flat app>
+      <v-toolbar-side-icon class="grey--text" @click="drawer=!drawer"></v-toolbar-side-icon>
       <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Todo</span>
         <span>Daily</span>
@@ -11,12 +13,19 @@
         <v-icon right>exit_to_app</v-icon>
       </v-btn>
     </v-toolbar>
+
+    <v-navigation-drawer app v-model="drawer" class="indigo"></v-navigation-drawer>
+
   </nav>
 </template>
 
 <script>
   export default {
-
+    data() {
+      return {
+        drawer: false
+      }
+    }
 
   }
 </script>
