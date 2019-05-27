bfaaa05b98dab9a50e18885df42a30645da81b5e
Thom David <thomdavid74@gmail.com>
2019-05-27 10:17:31 -0700

    Add new theme properties, colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -6,8 +6,11 @@ Vue.use(Vuetify, {
   iconfont: 'md',
   theme: {
     primary: '#9652ff',
+    secondary: '#36F1cd',
+    accent: '#39A0ED',
     success: '#3cd1c2',
-    info: '#ffaa2c',
+    warning: '#ffaa2c',
+    info: '#4C6085',
     error: '#f83e70'
   }
 })
