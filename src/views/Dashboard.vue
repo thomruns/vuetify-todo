fcd3ab99105a9b9434a1ef70ca81196ddd2ecaf9
Thom David <thomdavid74@gmail.com>
2019-05-28 10:06:53 -0700

    Project data sort functionality

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,6 +2,20 @@
   <div class="dashboard">
     <h1 class="subheading grey--text">Dashboard</h1>
     <v-container class="my-5">
+      <v-layout row class="mb-3">
+        <v-btn small flat color="grey" @click="sortBy('title')">
+          <v-icon left small>folder</v-icon>
+          <span class="caption text-lowercase">By Project Name</span>
+        </v-btn>
+        <v-btn small flat color="grey" @click="sortBy('person')">
+          <v-icon left small>person</v-icon>
+          <span class="caption text-lowercase">By person</span>
+        </v-btn>
+        <v-btn small flat color="grey" @click="sortBy('status')">
+          <v-icon left small>assignment</v-icon>
+          <span class="caption text-lowercase">By status</span>
+        </v-btn>
+      </v-layout>
       <!-- cycle through data and output -->
       <v-card flat v-for="project in projects" :key="project.title">
         <v-layout row wrap :class="`pa-3 project ${project.status}`">
@@ -40,6 +54,11 @@
         { title: 'Design video thumbnails', person: 'Janice', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
         { title: 'Create a community forum', person: 'Richard', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
         ]
+      }
+    },
+    methods: {
+      sortBy(prop) {
+        this.projects.sort((a, b) => a[prop] < b[prop] ? -1 : 1)
       }
     }
   }
