3781993efe95ace9e97c0c7df8cdcd41d2567b8e
Thom David <thomdavid74@gmail.com>
2019-05-28 09:14:41 -0700

    Dummy data and template output

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,25 +3,26 @@
     <h1 class="subheading grey--text">Dashboard</h1>
     <v-container class="my-5">
       
-      <v-card flat class="pa-3">
-        <v-layout row wrap>
+      <v-card flat v-for="project in projects" :key="project.title">
+        <v-layout row wrap :class="`pa-3 project ${project.status}`">
           <v-flex xs12 md6>
             <div class="caption grey--text">Project Title</div>
-            <div>Create a website</div>
+            <div>{{ project.title }}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
             <div class="caption grey--text">Person</div>
-            <div>Jack</div>
+            <div>{{ project.person }}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
             <div class="caption grey--text">Due By</div>
-            <div>1/6/19</div>
+            <div>{{ project.due }}</div>
           </v-flex>
           <v-flex xs2 sm4 md2>
             <div class="caption grey--text">Status</div>
-            <div>Overdue</div>
+            <div>{{ project.status }}</div>
           </v-flex>
         </v-layout>
+        <v-divider></v-divider>
       </v-card>
 
     </v-container>
@@ -34,9 +35,26 @@
   export default {
     data() {
       return {
-        
+        projects: [
+        { title: 'Design a new website', person: 'Thom', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Code up the homepage', person: 'Lisa', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Design video thumbnails', person: 'Janice', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Create a community forum', person: 'Richard', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        ]
       }
     }
 
   }
 </script>
+
+<style>
+  .project.complete {
+    border-left: 4px solid #3cd1c2;
+  }
+  .project.ongoing{
+  border-left: 4px solid #ffaa2c;
+  }
+  .project.overdue{
+    border-left: 4px solid #f83e70;
+  }
+</style>
