230f3d7280a319e716dfe4d8c0cbcd40e71b59af
Thom David <thomdavid74@gmail.com>
2019-05-28 08:54:33 -0700

    Add layout for project data display

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,7 +2,28 @@
   <div class="dashboard">
     <h1 class="subheading grey--text">Dashboard</h1>
     <v-container class="my-5">
-      content
+      
+      <v-card flat class="pa-3">
+        <v-layout row wrap>
+          <v-flex xs12 md6>
+            <div class="caption grey--text">Project Title</div>
+            <div>Create a website</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Person</div>
+            <div>Jack</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Due By</div>
+            <div>1/6/19</div>
+          </v-flex>
+          <v-flex xs2 sm4 md2>
+            <div class="caption grey--text">Status</div>
+            <div>Overdue</div>
+          </v-flex>
+        </v-layout>
+      </v-card>
+
     </v-container>
   </div>
   
@@ -11,6 +32,11 @@
 <script>
   
   export default {
+    data() {
+      return {
+        
+      }
+    }
 
   }
 </script>
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -2,7 +2,7 @@
   <div class="team">
     <h1 class="subheading grey--text">Team</h1>
     <v-container class="my-5">
-      content
+      
     </v-container>
   </div>
 </template>
