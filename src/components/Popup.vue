4117188621b43496171d3f21a85819ecbefd7c59
Thom David <thomdavid74@gmail.com>
2019-05-30 11:58:48 -0700

    Add new project form

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -5,14 +5,30 @@
       <v-card-title>
         <h2>Add a new project</h2>
       </v-card-title>
+      <v-card-text>
+        <v-form class="px-3">
+          <v-text-field label="title" v-model="title" prepend-icon="folder"></v-text-field>
+          <v-textarea label="Information" v-model="content" prepend-icon="edit"></v-textarea>
+          <v-btn flat class="success mx-0 mt-3" @click="submit">Add project</v-btn>
+        </v-form>
+      </v-card-text>
     </v-card>
-
   </v-dialog>
 </template>
 
 <script>
   export default {
-    
+    data() {
+      return {
+        title: '',
+        content: ''
+      }
+    },
+    methods: {
+      submit() {
+        console.log(this.title, this.content)
+      }
+    }
   }
 </script>
 
