7f06d83fdf82b815247a953a192bcdc8ffdf19d8
Thom David <thomdavid74@gmail.com>
2019-05-30 10:21:48 -0700

    Add dropdown menu for routes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,6 +7,20 @@
         <span>Daily</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+
+    <!-- dropdown menu -->
+    <v-menu offset-y>
+      <v-btn flat slot="activator" color="grey">
+        <v-icon left>expand_more</v-icon>
+        <span>menu</span>
+      </v-btn>
+      <v-list>
+        <v-list-tile v-for="link in links" :key="link.text" :to="link.route">
+          <v-list-tile-title>{{ link.text }}</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-menu>
+
       <v-btn flat color="grey">
         <span>Sign Out</span>
         <v-icon right>exit_to_app</v-icon>
